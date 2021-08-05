82dd5383494dee0d939aac6b8e271c06dcd6bb17
gonnichiwa <keepthefaith00@gmail.com>
2021-08-05 14:00:12 +0900

    불필요 import 삭제

--- src/kr/ac/dgd/Main.java
+++ src/kr/ac/dgd/Main.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Scanner;
 
 import kr.ac.dgd.input.InputProcess;
 
